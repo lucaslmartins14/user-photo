91d77ae2623bd723bb0cffaa9d1c487c95b33986
Lucas Martins <lucaslmartins14@gmail.com>
2020-03-02 13:08:24 -0300

    Criando menu inferior

---
+++ app/app/src/main/java/com/example/users_and_photos/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.users_and_photos.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
